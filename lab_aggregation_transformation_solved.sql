c6ce1f5bf97cb88d3f34007eee7f684ee2e87d42
Sergio Pizzolante <sergio.pizzolante95@gmail.com>
2024-03-12 22:26:49 +0100

    LAB SOLVED

---
+++ lab_aggregation_transformation_solved.sql
@@ -0,0 +1,95 @@
+USE SAKILA;
+
+-- 1. You need to use SQL built-in functions to gain insights relating to the duration of movies:
+
+-- 1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
+SELECT MIN(length) AS min_duration, MAX(length) AS max_duration
+ FROM sakila.film;
+ 
+ -- 1.2  Express the average movie duration in hours and minutes. Don't use decimals.
+SELECT CONCAT(
+AVG(length) DIV 60, ' hours ',
+FLOOR(AVG(length) % 60), ' minutes ')
+AS avg_duration
+FROM sakila.film;
+ 
+-- 2. You need to gain insights related to rental dates:
+
+-- 2.1 Calculate the number of days that the company has been operating
+SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) FROM sakila.rental;
+
+-- 2.2 Retrieve rental information and add two additional columns to show the month and weekday of the rental.
+-- Return 20 rows of results.
+SELECT *, MONTH(rental_date) AS rental_month, DAYNAME(rental_date) AS rental_weekday
+FROM sakila.rental
+LIMIT 20;
+
+-- 2.3 Retrieve rental information and add an additional column called DAY_TYPE with values 'weekend' or 
+-- 'workday', depending on the day of the week
+SELECT *, 
+	CASE
+		WHEN DAYNAME(rental_date) IN ('Saturday', 'Sunday') THEN 'weekend'
+		ELSE 'workday'
+    END AS DAY_TYPE
+FROM sakila.rental;
+
+
+-- 3. You need to ensure that customers can easily access information about the movie collection. 
+-- To achieve this, retrieve the film titles and their rental duration. If any rental duration value is NULL, 
+-- replace it with the string 'Not Available'. Sort the results of the film title in ascending order.
+SELECT title, 
+	IFNULL(rental_duration, 'Not Available') AS rental_duration
+FROM sakila.film
+ORDER BY title;
+
+
+-- BONUS
+-- 4. The marketing team for the movie rental company now needs to create a personalized email campaign 
+-- for customers. To achieve this, you need to retrieve the concatenated first and last names of customers, 
+-- along with the first 3 characters of their email address, so that you can address them by their first name 
+-- and use their email address to send personalized recommendations. The results should be ordered by last name 
+-- in ascending order to make it easier to use the data
+
+SELECT first_name, last_name, email FROM sakila.customer ORDER BY last_name ASC;
+
+
+-- ---------------------------------------------------
+-- ------------------- CHALLENGE 2 -------------------
+-- ---------------------------------------------------
+
+-- 1. Next, you need to analyze the films in the collection to gain some more insights. Using the film table, determine:
+
+-- 1.1 The total number of films that have been released
+
+SELECT count(*) FROM sakila.film;
+
+-- 1.2 The number of films for each rating
+
+SELECT COUNT(rating),rating FROM sakila.film GROUP BY rating;
+
+-- 1.3 The number of films for each rating, sorting the results in descending order of the number of films. 
+-- This will help you to better understand the popularity of different film ratings and adjust purchasing 
+-- decisions accordingly.
+
+SELECT COUNT(rating),rating FROM sakila.film GROUP BY rating ORDER BY COUNT(rating) DESC;
+
+
+
+-- 2. Using the film table, determine:
+
+-- 2.1 The mean film duration for each rating, and sort the results in descending order of the mean duration. 
+-- Round off the average lengths to two decimal places. This will help identify popular movie lengths for each category.
+
+SELECT COUNT(rating), rating, ROUND(AVG(length),2) FROM sakila.film GROUP BY rating ORDER BY AVG(length) DESC;
+
+-- 2.2 Identify which ratings have a mean duration of over two hours in order to help select films for customers
+--  who prefer longer movies.
+
+SELECT COUNT(rating), rating, ROUND(AVG(length),2) 
+FROM sakila.film 
+GROUP BY rating 
+HAVING AVG(length) > 119
+ORDER BY AVG(length) DESC;
+
+-- 3. Bonus: determine which last names are not repeated in the table actor.
+
